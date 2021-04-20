a2fe0d1449bcc50cf203fe8d4c5c4310b49ee963
Theryston <funktodo2@gmail.com>
2021-04-20 13:36:14 -0300

    remove err

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,15 @@
 import express from 'express'
 import { routes } from './routes' 
 import './database'
+import bodyParser from 'body-parser'
 
 const app = express()
+
+//body parser
+app.use(bodyParser.urlencoded({
+	extended: false
+}));
+app.use(bodyParser.json())
 
 app.use(routes)
 
